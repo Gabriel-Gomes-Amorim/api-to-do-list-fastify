e0dd1838d83bd367bbdc22f2b5f36733d378f211
Gabriel-Gomes-Amorim <gomez.amorim18@hotmail.com>
2024-03-20 19:02:36 -0300

    estruturando db

---
+++ prisma/migrations/20240320215155_create_table_task/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "task" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
